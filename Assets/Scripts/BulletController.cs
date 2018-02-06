8ce34d4d08513b8b96eb9606c0d0c99e21e21365
三浦亮二 <miura@chatlune.jp>
2018-02-06 11:47:31 +0900

    FirstCommit

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	float power;
+	float rand;
+
+	void Start(){
+		power = 5.0f;
+		GetComponent<Rigidbody> ().AddForce (0, power, 0, ForceMode.Impulse);
+		rand = Random.Range (-0.1f, 0.2f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (rand, 0, 0,Space.World);
+		transform.Rotate(new Vector3(15,0,15));
+		if (transform.position.y < -5.5f) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/BulletGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGenerator : MonoBehaviour {
+
+    public GameObject bullet;
+    float time;
+	float seed;
+
+    // Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time >= 0.3f) {
+			Instantiate (bullet,transform.position,transform.rotation);	
+			time = 0;
+		}
+		seed += Time.deltaTime;
+		transform.position = new Vector3 (Mathf.Sin (seed)*5.5f, transform.position.y, transform.position.z);
+	}
+}
